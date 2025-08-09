8370fce3e706884b00c471a14cb380c00cc0b2e5
arturcast <acastilla@gascaribe.com>
2025-08-08 23:02:10 -0500

    refactor: remove unused dashboard_id output from outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,7 +2,3 @@ output "job_id" {
   description = "ID del job creado"
   value       = databricks_job.worldbank.id
 }
-output "dashboard_id" {
-  description = "ID del dashboard SQL creado"
-  value       = databricks_sql_dashboard.wb_dashboard.id
-}
