bfb026c91c3c2f5d9fe29874f01d24cc0724961d
arturcast <acastilla@gascaribe.com>
2025-08-10 21:20:57 -0500

    fix(terraform): environment (singular) + serverless env + cron

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,17 +7,17 @@ terraform {
   }
 }
 
-# El provider toma DATABRICKS_HOST y DATABRICKS_TOKEN del entorno (GitHub Actions)
+# El provider usa DATABRICKS_HOST y DATABRICKS_TOKEN de las variables de entorno
 provider "databricks" {}
 
 locals {
   indicators_csv = join(",", var.indicators)
 
-  # var.cron_time_bogota viene "HH:MM" (ej: "02:00")
+  # var.cron_time_bogota viene en formato "HH:MM" (ej. "02:00")
   cron_hour   = tonumber(element(split(":", var.cron_time_bogota), 0))
   cron_minute = tonumber(element(split(":", var.cron_time_bogota), 1))
 
-  # Quartz: sec min hour day-of-month month day-of-week ?  (diario)
+  # Quartz: sec min hour day-of-month month day-of-week ?  -> diario
   quartz_expr = "0 ${local.cron_minute} ${local.cron_hour} * * ?"
 }
 
@@ -37,12 +37,12 @@ resource "databricks_job" "worldbank" {
     pause_status           = "UNPAUSED"
   }
 
-  # Declaración de environment serverless (requerido para Python tasks en serverless)
-  environments {
+  # Declaración del environment (requerido para Python tasks en serverless)
+  environment {
     environment_key = "srvless"
     spec = jsonencode({
       client              = "serverless"
-      environment_version = "3"   # si tu workspace no soporta v3, cambia a "2"
+      environment_version = "2"   # usa "3" si tu workspace la soporta
     })
   }
 
@@ -64,6 +64,7 @@ resource "databricks_job" "worldbank" {
   task {
     task_key        = "silver_transform"
     environment_key = "srvless"
+
     depends_on { task_key = "bronze_ingestion" }
 
     spark_python_task {
@@ -75,6 +76,7 @@ resource "databricks_job" "worldbank" {
   task {
     task_key        = "gold_aggregate"
     environment_key = "srvless"
+
     depends_on { task_key = "silver_transform" }
 
     spark_python_task {
